8ac370e6637149d5aac5af5ecb33ca9d798e4d1b
choikangx <choikangx@gmail.com>
2019-02-18 17:11:13 +0900

    1day study
    openapi oauth2

---
+++ openapi/contactweb/src/main/java/com/multi/contactsapp/domain/Contact.java
@@ -0,0 +1,49 @@
+package com.multi.contactsapp.domain;
+
+public class Contact {
+	private int no;
+	private String name;
+	private String tel;
+	private String address;
+	//생성자 setter / getter 자동생성
+	
+	
+	public Contact(int no, String name, String tel, String address) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.tel = tel;
+		this.address = address;
+	}
+	public Contact() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
